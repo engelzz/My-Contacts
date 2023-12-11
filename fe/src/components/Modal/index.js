74f42840bf4199c09783911b8182530f188a56b0
Matheus Engel <matheusribeiroengel@outlook.com>
2023-12-11 18:41:08 -0300

    feat: Home page setup

---
+++ fe/src/components/Modal/index.js
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+import { Container, Footer, Overlay } from './styles';
+
+import Button from '../Button';
+
+export default function Modal({ danger }) {
+  return ReactDOM.createPortal(
+    <Overlay>
+      <Container danger={danger}>
+        <h1> Título </h1>
+        <p>
+          Call to action
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+
+          <Button type="button" danger={danger}>
+            Deletar
+          </Button>
+
+        </Footer>
+      </Container>
+    </Overlay>,
+    document.getElementById('modal-root'),
+  );
+}
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
